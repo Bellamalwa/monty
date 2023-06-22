772dfbd2410439d671668c303ad389c809c302fb
Bellamalwa <duchybella@gmail.com>
2023-06-22 09:16:02 -0700

    monty

--- monty.h
+++ monty.h
@@ -1,17 +1,14 @@
-#ifndef _MONTY_HOL
-#define _MONTY_HOL
+#ifndef MONTY_H
+#define MONTY_H
 
-/* Libraries */
-#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
-#include <ctype.h>
-
-#define LIMITER " \n"
-
-
-/* Structures Doubly linked lists */
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -20,13 +17,13 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,31 +32,35 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Prototypes basic operations */
-
-void pall(stack_t **head, unsigned int counter);
-void push(stack_t **head, unsigned int counter);
-void pint(stack_t **head, unsigned int counter);
-void swap(stack_t **head, unsigned int counter);
-void pop(stack_t **head, unsigned int counter);
-void nop(stack_t **head, unsigned int counter);
-void add(stack_t **head, unsigned int counter);
-void sub(stack_t **head, unsigned int counter);
-void mul(stack_t **head, unsigned int counter);
-void _div(stack_t **head, unsigned int counter);
-
-/* Selector structure */
-void select_func(stack_t **head, char *token, unsigned int counter);
-
-/* Free memory */
-void free_all(stack_t **head);
-
-#endif /* _MONTY_HOL */
+#define SIZE 100
+#define STACKS 0
+#define QUEUES 1
+int stack[SIZE];
+/**
+ * struct variable_s - struct to contain the main variables of the Monty interpreter
+ * @queues: flag to determine if in stack vs queue mode
+ * @stack_length: length of the stack
+ */
+typedef struct variable_s
+{
+	int queues;
+	size_t stack_length;
+} variable_t;
+extern variable_t variable;
+stack_t *new_node_create(stack_t **stack, const int n);
+int check_for_digit(char *str);
+void _push(stack_t **stack, unsigned int line_number);
+int main(int argc, char *argv[]);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t **stack);
+void _pop(stack_t **stack, __attribute__((unused))unsigned int line_number);
+#endif
